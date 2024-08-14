df91e66591057051ed614eb63c9ea749fdb1362a
Mr.喜 <863450594@qq.com>
2024-08-14 17:44:54 +0800

    添加项目文件。

---
+++ Components/Common.cs
@@ -0,0 +1,9 @@
+﻿namespace HexoArticleEditor.Components
+{
+    public class Common
+    {
+        public static Action<bool> DarkThemeChanged { get; set; }
+
+        public static bool IsDarkMode { get; set; } = true;
+    }
+}
