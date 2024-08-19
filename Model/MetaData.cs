08d027b55e2045c71fb4d9c2c268a6fb977be4b4
Mr.喜 <863450594@qq.com>
2024-08-19 17:53:51 +0800

    元数据编辑器

---
+++ Model/MetaData.cs
@@ -0,0 +1,17 @@
+﻿namespace HexoArticleEditor.Model
+{
+    public class MetaData
+    {
+        public string Title { get; set; } = "未命名";
+
+        public List<string> Tags { get; set; } = [];
+
+        public List<string> Categories { get; set; } = [];
+
+        public string Cover { get; set; } = "";
+
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+
+        public TimeSpan? CreateTime { get; set; } = new TimeSpan(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+    }
+}
