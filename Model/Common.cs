85c042a421f30a4e5cd9a8685ca1d35b4a600fc2
Mr.喜 <863450594@qq.com>
2024-08-29 17:28:38 +0800

    移动Common文件至Model文件夹

---
+++ Model/Common.cs
@@ -0,0 +1,48 @@
+﻿namespace HexoArticleEditor.Model
+{
+    public class Common
+    {
+        public static event Action<bool>? DarkThemeChanged;
+
+        public static event Action? ArticleListClicked;
+
+        public static event Action? TerminalClicked;
+
+        public static event Action<Article>? OnCurrentArticleChanged;
+
+        public static event Action<Article, bool>? OnArticleSaveStateChanged;
+
+        public static void InvokeDarkModeChanged(bool darkMode)
+        {
+            DarkThemeChanged?.Invoke(darkMode);
+        }
+
+        public static void InvokeArticleListClicked()
+        {
+            ArticleListClicked?.Invoke();
+        }
+
+        public static void InvokeTerminalClicked()
+        {
+            TerminalClicked?.Invoke();
+        }
+
+        public static void InvokeCurrentArticleChanged(Article? article)
+        {
+            if (article != null) 
+            {
+                OnCurrentArticleChanged?.Invoke(article);
+            }
+        }
+
+        public static void InvokeArticleSaveStateChanged(Article? article, bool saveState)
+        {
+            if (article != null) 
+            {
+                OnArticleSaveStateChanged?.Invoke(article, saveState);
+            }
+        }
+
+        public static bool IsDarkMode { get; set; } = true;
+    }
+}
