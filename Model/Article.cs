4b152548a745d889c3282ec50b008140a90cc298
Mr.喜 <863450594@qq.com>
2024-08-29 17:29:04 +0800

    实现文件列表拉取

---
+++ Model/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace HexoArticleEditor.Model
+{
+    public class Article
+    {
+        public string Name { get; set; } = "";
+
+        public string FilePath { get; set; } = "";
+
+        public bool Saved { get; set; } = true;
+
+        public MetaData? MetaData { get; set; } = new MetaData();
+    }
+}
