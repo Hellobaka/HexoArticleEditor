1fbc2da932eb6ff562c04716edfea9998c4ed7ef
Mr.喜 <863450594@qq.com>
2024-09-02 17:46:48 +0800

    选择回滚节点窗口

---
+++ Model/AutoSaveItem.cs
@@ -0,0 +1,13 @@
+﻿namespace HexoArticleEditor.Model
+{
+    public class AutoSaveItem
+    {
+        public string ParentArticleName { get; set; } = "";
+
+        public string FilePath { get; set; } = "";
+
+        public string FileName => Path.GetFileNameWithoutExtension(FilePath);
+
+        public string DisplayName { get; set; } = "";
+    }
+}
