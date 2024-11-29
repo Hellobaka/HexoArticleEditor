a1fafe247f1dc53f3a216d8ea615f9e8820e46ef
Mr.喜 <863450594@qq.com>
2024-11-29 09:43:59 +0800

    文章列表按修改日期排序

--- Model/Article.cs
+++ Model/Article.cs
@@ -12,4 +12,18 @@
 
         public MetaData MetaData { get; set; } = new MetaData();
     }
+
+    public class ArticleComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            if (string.IsNullOrEmpty(x) || string.IsNullOrEmpty(y)
+                || !File.Exists(x) || !File.Exists(y))
+            {
+                return 0;
+            }
+
+            return new FileInfo(x).LastWriteTime.CompareTo(new FileInfo(y).LastWriteTime);
+        }
+    }
 }
